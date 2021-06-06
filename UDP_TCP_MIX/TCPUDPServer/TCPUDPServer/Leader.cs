3c2e6271c9de3a40569dd469cc0969b3769eb6cd
ai-lintran1510 <82454307+ai-lintran1510@users.noreply.github.com>
2021-06-06 16:59:09 +0200

    Add files via upload

---
+++ UDP_TCP_MIX/TCPUDPServer/TCPUDPServer/Leader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCPUDPServer
+{
+    static class Leader
+    {
+        public static String LeaderHostName { get; set; }
+        public static int PortNr { get; set; }
+        public static int MyPID { get; set; }
+        public static bool ImLeader = false;
+    }
+}
